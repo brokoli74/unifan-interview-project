47e5018cce0488577145212c5be6f542df48b3b2
Сергей <zo1nks@mail.ru>
2023-06-06 00:28:15 +0300

    Initial commit

---
+++ src/main/java/com/example/unifanproject/UnifanProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.unifanproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnifanProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnifanProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/unifanproject/UnifanProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unifanproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnifanProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
